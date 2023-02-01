f138086049e84bcae4b2d287e047200bf95b251f
suttikaet chanya <36092947+iceze087@users.noreply.github.com>
2023-02-01 16:03:27 +0700

    w

--- app.py
+++ app.py
@@ -140,18 +140,23 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        #     return redirect(request.url)
+        file1 = request.files['p1']
+        file2 = request.files['p2']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        if file.filename == '':
+        if file1.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if file1 and allowed_file(file1.filename):
+            filename = secure_filename(file1.filename)
+            file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+        if file2 and allowed_file(file2.filename):
+            filename = secure_filename(file2.filename)
+            file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('upload_file', name=filename))
     return render_template("6imageupload.html")
 
